80701bb43068210996ac7165792b01bf767c21c5
Manhhung114 <hoangmanhhung2018@gmail.com>
2026-09-16 21:53:37 +0700

    refactor(cleanup): remove retired signed-adjustment facade

--- src/qlda/runtime_core/project_cost_signed_adjustments.py
+++
@@ -1,6 +0,0 @@
-"""Compatibility facade; signed finance logic is consolidated in finance_consistency."""
-from qlda.runtime_core.finance_consistency import (
-    install_finance_consistency_core as install_project_cost_signed_adjustments,
-)
-
-__all__ = ["install_project_cost_signed_adjustments"]
