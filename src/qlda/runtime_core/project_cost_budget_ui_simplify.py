6a3310946e678f877ca7e327415179b242bab2da
Manhhung114 <hoangmanhhung2018@gmail.com>
2026-09-16 21:53:26 +0700

    refactor(cleanup): remove retired project cost UI facade

--- src/qlda/runtime_core/project_cost_budget_ui_simplify.py
+++
@@ -1,6 +0,0 @@
-"""Compatibility facade; implementation consolidated in finance_consistency."""
-from qlda.runtime_core.finance_consistency import (
-    install_finance_consistency_ui as install_project_cost_budget_ui_simplify,
-)
-
-__all__ = ["install_project_cost_budget_ui_simplify"]
